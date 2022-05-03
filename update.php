94009732fb4722f46f428ecce0992b9f33e852e2
Chandra Keerthi <ck261414@gmail.com>
2022-05-03 17:28:05 -0400

    Update update.php

--- update.php
+++ update.php
@@ -1,12 +1,8 @@
 <html>
-<body>
-  
 <form action="update2.php" method="post">
 <h1>Update NBA Player</h1>
 <label for="name">Name: </label>
 <input type="text" id="name" name="name">
 <input type="submit" value="Search"/>
-
-</body>
 </form>
 </html>
